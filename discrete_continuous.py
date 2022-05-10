f9c02a99ced1f2c5252dfcf641963dbccfaa8d81
Malik Boudiaf <mboudiaf@Maliks-MacBook-Pro.local>
2022-05-10 15:19:09 +0200

    interface hybrid estim with discrete-discrete

--- discrete_continuous.py
+++ discrete_continuous.py
@@ -3,11 +3,12 @@ from typing import Any, Union
 from copy import deepcopy
 from torch import Tensor
 import torch
+from .discret_discret_similarity import DiscreteEstimator
 
 
 class LinearEstimator(HybridEstimator):
 
-    def __init__(self, classifier: Any):
+    def __init__(self, classifier: Any, entropy_estimator: DiscreteEstimator):
         """
         Parameters
         ----------
@@ -18,6 +19,7 @@ class LinearEstimator(HybridEstimator):
         assert hasattr(classifier, 'predict_proba') and callable(getattr(classifier, 'predict_proba'))
         self.initial_classifier = classifier
         self.classifier = deepcopy(classifier)
+        self.entropy_estimator = entropy_estimator
         self.epsilon = 1e-9
 
     def reset(self):
@@ -29,8 +31,8 @@ class LinearEstimator(HybridEstimator):
     def predict(self, X: Tensor, Y: Tensor):
 
         self.classifier.fit(X, Y)
-        y_probas = self.classifier.predict_proba(X)
-        cond_ent = - (y_probas * torch.log(y_probas + self.epsilon)).sum(-1)
-        marg_prob = y_probas.mean(0)
-        marg_ent = - (marg_prob * torch.log(marg_prob + self.epsilon)).sum()
+        y_cond_probas = self.classifier.predict_proba(X)
+        y_marg_probas = y_cond_probas.mean(0)
+        cond_ent = self.entropy_estimator.predict(y_cond_probas).mean(0)
+        marg_ent = self.entropy_estimator.predict(y_marg_probas)
         return marg_ent - cond_ent
