1e611db5a7813b8bea44ddb1098a0d5023b57728
PierreColombo <colombo.pierre@gmail.com>
2022-05-28 11:34:24 +0100

    differential entropy benchmark + estimators working

--- continuous_measures.py
+++ continuous_measures.py
@@ -1,20 +1,8 @@
-import numpy as np
-import math
-
-import torch
-import torch.nn as nn
 import copy
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import time
-import seaborn as sns
-from tensorboardX import SummaryWriter
 from continuous_estimator_helpers import *
 from abstract_class import ContinuousEstimator
 import torch.nn as nn
 from utils import compute_mean, compute_cov
-from geomloss import SamplesLoss
-from continuous_self import *
 
 
 ################################################
@@ -108,6 +96,159 @@ class MGHClosedFRECHET(ContinuousEstimator):
         self.hypo_cov = compute_cov(hypo_dist)
 
 
+###############################################
+####### Differential Entropy Estimators #######
+###############################################
+
+class DOE(nn.Module):
+    def __init__(self, dim, hidden, pdf='gauss'):
+        """
+        paper from
+        :param dim:
+        :param hidden:
+        :param pdf:
+        """
+        super(DOE, self).__init__()
+        self.qY = PDF(dim, pdf)
+        self.qY_X = ConditionalPDF(dim, hidden, pdf)
+
+    def learning_loss(self, X, Y):
+        hY = self.qY(Y)
+        hY_X = self.qY_X(Y, X)
+
+        loss = hY + hY_X
+        return loss
+
+    def forward(self, X, Y):
+        hY = self.qY(Y)
+        hY_X = self.qY_X(Y, X)
+
+        return hY - hY_X
+
+
+class KNIFE(nn.Module):
+    def __init__(self, args, zc_dim, zd_dim, init_samples=None):
+
+        self.optimize_mu = args.optimize_mu
+        self.K = args.marg_modes if self.optimize_mu else args.batch_size
+        self.d = zc_dim
+        self.use_tanh = args.use_tanh
+        self.init_std = args.init_std
+        super(KNIFE, self).__init__()
+
+        self.logC = torch.tensor([-self.d / 2 * np.log(2 * np.pi)])
+
+        if init_samples is None:
+            init_samples = self.init_std * torch.randn(self.K, self.d)
+        # self.means = nn.Parameter(torch.rand(self.K, self.d), requires_grad=True)
+        if self.optimize_mu:
+            self.means = nn.Parameter(init_samples, requires_grad=True)  # [K, db]
+        else:
+            self.means = nn.Parameter(init_samples, requires_grad=False)
+
+        if args.cov_diagonal == 'var':
+            diag = self.init_std * torch.randn((1, self.K, self.d))
+        else:
+            diag = self.init_std * torch.randn((1, 1, self.d))
+        self.logvar = nn.Parameter(diag, requires_grad=True)
+
+        if args.cov_off_diagonal == 'var':
+            tri = self.init_std * torch.randn((1, self.K, self.d, self.d))
+            tri = tri.to(init_samples.dtype)
+            self.tri = nn.Parameter(tri, requires_grad=True)
+        else:
+            self.tri = None
+
+        weigh = torch.ones((1, self.K))
+        if args.average == 'var':
+            self.weigh = nn.Parameter(weigh, requires_grad=True)
+        else:
+            self.weigh = nn.Parameter(weigh, requires_grad=False)
+
+    def logpdf(self, x):
+        assert len(x.shape) == 2 and x.shape[1] == self.d, 'x has to have shape [N, d]'
+        x = x[:, None, :]
+        w = torch.log_softmax(self.weigh, dim=1)
+        y = x - self.means
+        logvar = self.logvar
+        if self.use_tanh:
+            logvar = logvar.tanh()
+        var = logvar.exp()
+        y = y * var
+        # print(f"Marg : {var.min()} | {var.max()} | {var.mean()}")
+        if self.tri is not None:
+            y = y + torch.squeeze(torch.matmul(torch.tril(self.tri, diagonal=-1), y[:, :, :, None]), 3)
+        y = torch.sum(y ** 2, dim=2)
+
+        y = -y / 2 + torch.sum(torch.log(torch.abs(var) + 1e-8), dim=-1) + w
+        y = torch.logsumexp(y, dim=-1)
+        return self.logC.to(y.device) + y
+
+    def update_parameters(self, z):
+        self.means = z
+
+    def forward(self, x):
+        y = -self.logpdf(x)
+        return torch.mean(y)
+
+
+
+class ConditionalKNIFE(nn.Module):
+
+    def __init__(self, device,
+                 number_of_samples,  # [K, d]
+                 x_size, y_size,
+                 layers=1,
+                 ):
+        super(ConditionalKNIFE, self).__init__()
+        self.K, self.d = number_of_samples, y_size
+        self.device = device
+
+        self.logC = torch.tensor([-self.d / 2 * np.log(2 * np.pi)]).to(self.device)
+
+        # mean_weight = 10 * (2 * torch.eye(K) - torch.ones((K, K)))
+        # mean_weight = _c(mean_weight[None, :, :, None])  # [1, K, K, 1]
+        # self.mean_weight = nn.Parameter(mean_weight, requires_grad=True)
+
+        self.std = FF(self.d, self.d * 2, self.K, layers)
+        self.weight = FF(self.d, self.d * 2, self.K, layers)
+        # self.mean_weight = FF(d, hidden, K**2, layers)
+        self.mean_weight = FF(self.d, self.d * 2, self.K * x_size, layers)
+        self.x_size = x_size
+
+    def _get_mean(self, y):
+        # mean_weight = self.mean_weight(y).reshape((-1, self.K, self.K, 1))  # [N, K, K, 1]
+        # means = torch.sum(torch.softmax(mean_weight, dim=2) * self.base_X, dim=2)  #[1, K, d]
+        means = self.mean_weight(y).reshape((-1, self.K, self.x_size))  # [N, K, d]
+        return means
+
+    def logpdf(self, x, y):  # H(X|Y)
+        # for data in (x, y):
+        # assert len(data.shape) == 2 and data.shape[1] == self.d, 'x has to have shape [N, d]'
+        # assert x.shape == y.shape, "x and y need to have the same shape"
+
+        x = x[:, None, :]  # [N, 1, d]
+
+        w = torch.log_softmax(self.weight(y), dim=-1)  # [N, K]
+        std = self.std(y).exp()  # [N, K]
+        # std = self.std(y)  # [N, K]
+        mu = self._get_mean(y)  # [1, K, d]
+
+        y = x - mu  # [N, K, d]
+        y = std ** 2 * torch.sum(y ** 2, dim=2)  # [N, K]
+
+        y = -y / 2 + self.d * torch.log(torch.abs(std)) + w
+        y = torch.logsumexp(y, dim=-1)
+        return self.logC + y
+
+    def pdf(self, x, y):
+        return torch.exp(self.logpdf(x, y))
+
+    def forward(self, x, y):
+        z = -self.logpdf(x, y)
+        return torch.mean(z)
+
+
 #############################################
 ####### Mutual Information Estimators #######
 #############################################
@@ -115,6 +256,7 @@ class MGHClosedFRECHET(ContinuousEstimator):
 
 class MGHClosedMI(ContinuousEstimator):
     def __init__(self, name):
+        raise NotImplementedError
         self.name = name
 
     def predict(self, ref_dist, hypo_dist):
@@ -130,7 +272,6 @@ class MGHClosedMI(ContinuousEstimator):
     def fit(self, ref_dist, hypo_dist):
         self.ref_cov = compute_cov(ref_dist)
         self.hypo_cov = compute_cov(hypo_dist)
-
 
 
 class CLUB(nn.Module):  # CLUB: Mutual Information Contrastive Learning Upper Bound
@@ -409,12 +550,12 @@ class MIKernelEstimator(nn.Module):
         self.use_joint = use_joint
         self.count = 0
         self.count_learning = 0
-        self.kernel_1 = MultiGaussKernelEE(device, number_of_samples, x_size)
+        self.kernel_1 = KNIFE(device, number_of_samples, x_size)
         if self.use_joint:
-            self.kernel_2 = MultiGaussKernelEE(device, number_of_samples, y_size)
-            self.kernel_joint = MultiGaussKernelEE(device, number_of_samples, x_size + y_size)
+            self.kernel_2 = KNIFE(device, number_of_samples, y_size)
+            self.kernel_joint = KNIFE(device, number_of_samples, x_size + y_size)
         else:
-            self.kernel_conditional = MultiGaussKernelCondEE(device, number_of_samples, x_size, y_size)
+            self.kernel_conditional = ConditionalKNIFE(device, number_of_samples, x_size, y_size)
         # self.kernel_joint.joint = True
 
     def forward(self, x_samples, y_samples):  # samples have shape [sample_size, dim]
@@ -448,210 +589,7 @@ class MIKernelEstimator(nn.Module):
             return hz_1 + hz_g1
 
 
-
-
-
-class DOE(nn.Module):
-    def __init__(self, dim, hidden, pdf='gauss'):
-        """
-        paper from
-        :param dim:
-        :param hidden:
-        :param pdf:
-        """
-        super(DOE, self).__init__()
-        self.qY = PDF(dim, pdf)
-        self.qY_X = ConditionalPDF(dim, hidden, pdf)
-
-    def learning_loss(self, X, Y):
-        hY = self.qY(Y)
-        hY_X = self.qY_X(Y, X)
-
-        loss = hY + hY_X
-        return loss
-
-    def forward(self, X, Y):
-        hY = self.qY(Y)
-        hY_X = self.qY_X(Y, X)
-
-        return hY - hY_X
-
-
-class MultiGaussKernelEE(nn.Module):
-    def __init__(self, device, number_of_samples, hidden_size,
-                 # [K, d] to initialize the kernel :) so K is the number of points :)
-                 average='weighted',  # un
-                 cov_diagonal='var',  # diagonal of the covariance
-                 cov_off_diagonal='var',  # var
-                 ):
-
-        self.K, self.d = number_of_samples, hidden_size
-        super(MultiGaussKernelEE, self).__init__()
-        self.device = device
-
-        # base_samples.requires_grad = False
-        # if kernel_positions in ('fixed', 'free'):
-        #    self.mean = base_samples[None, :, :].to(self.args.device)
-        # else:
-        #    self.mean = base_samples[None, None, :, :].to(self.args.device)  # [1, 1, K, d]
-
-        # self.K = K
-        # self.d = d
-        self.joint = False
-
-        self.logC = torch.tensor([-self.d / 2 * np.log(2 * np.pi)]).to(
-            self.device)
-
-        self.means = nn.Parameter(torch.rand(number_of_samples, hidden_size), requires_grad=True).to(
-            self.device)
-        if cov_diagonal == 'const':
-            diag = torch.ones((1, 1, self.d))
-        elif cov_diagonal == 'var':
-            diag = torch.ones((1, self.K, self.d))
-        else:
-            assert False, f'Invalid cov_diagonal: {cov_diagonal}'
-        self.diag = nn.Parameter(diag.to(self.device))
-
-        if cov_off_diagonal == 'var':
-            tri = torch.zeros((1, self.K, self.d, self.d))
-            self.tri = nn.Parameter(tri.to(self.device))
-        elif cov_off_diagonal == 'zero':
-            self.tri = None
-        else:
-            assert False, f'Invalid cov_off_diagonal: {cov_off_diagonal}'
-
-        self.weigh = torch.ones((1, self.K), requires_grad=False).to(self.device)
-        if average == 'weighted':
-            self.weigh = nn.Parameter(self.weigh, requires_grad=True)
-        else:
-            assert average == 'fixed', f"Invalid average: {average}"
-
-    def logpdf(self, x, y=None):
-        assert len(x.shape) == 2 and x.shape[1] == self.d, 'x has to have shape [N, d]'
-        x = x[:, None, :]
-        w = torch.log_softmax(self.weigh, dim=1)
-        y = x - self.means
-        if self.tri is not None:
-            y = y * self.diag + torch.squeeze(torch.matmul(torch.tril(self.tri, diagonal=-1), y[:, :, :, None]), 3)
-        else:
-            y = y * self.diag
-        y = torch.sum(y ** 2, dim=2)
-
-        y = -y / 2 + torch.sum(torch.log(torch.abs(self.diag)), dim=2) + w
-        if self.joint:
-            y = torch.log(torch.sum(torch.exp(y), dim=-1) / 2)
-        else:
-            y = torch.logsumexp(y, dim=-1)
-        return self.logC + y
-
-    def learning_loss(self, x_samples, y=None):
-        return -self.forward(x_samples)
-
-    def update_parameters(self, kernel_dict):
-        tri = []
-        means = []
-        weigh = []
-        diag = []
-        for key, value in kernel_dict.items():  # detach and clone
-            tri.append(copy.deepcopy(value.tri.detach().clone()))
-            means.append(copy.deepcopy(value.means.detach().clone()))
-            weigh.append(copy.deepcopy(value.weigh.detach().clone()))
-            diag.append(copy.deepcopy(value.diag.detach().clone()))
-
-        self.tri = nn.Parameter(torch.cat(tri, dim=1), requires_grad=True).to(self.device)
-        self.means = nn.Parameter(torch.cat(means, dim=0), requires_grad=True).to(self.device)
-        self.weigh = nn.Parameter(torch.cat(weigh, dim=-1), requires_grad=True).to(self.device)
-        self.diag = nn.Parameter(torch.cat(diag, dim=1), requires_grad=True).to(self.device)
-
-    def pdf(self, x):
-        return torch.exp(self.logpdf(x))
-
-    def forward(self, x, y=None):
-        y = torch.abs(-self.logpdf(x))
-        return torch.mean(y)
-
-
-class MultiGaussKernelCondEE(nn.Module):
-
-    def __init__(self, device,
-                 number_of_samples,  # [K, d]
-                 x_size, y_size,
-                 layers=1,
-                 ):
-        super(MultiGaussKernelCondEE, self).__init__()
-        self.K, self.d = number_of_samples, y_size
-        self.device = device
-
-        self.logC = torch.tensor([-self.d / 2 * np.log(2 * np.pi)]).to(self.device)
-
-        # mean_weight = 10 * (2 * torch.eye(K) - torch.ones((K, K)))
-        # mean_weight = _c(mean_weight[None, :, :, None])  # [1, K, K, 1]
-        # self.mean_weight = nn.Parameter(mean_weight, requires_grad=True)
-
-        self.std = FF(self.d, self.d * 2, self.K, layers)
-        self.weight = FF(self.d, self.d * 2, self.K, layers)
-        # self.mean_weight = FF(d, hidden, K**2, layers)
-        self.mean_weight = FF(self.d, self.d * 2, self.K * x_size, layers)
-        self.x_size = x_size
-
-    def _get_mean(self, y):
-        # mean_weight = self.mean_weight(y).reshape((-1, self.K, self.K, 1))  # [N, K, K, 1]
-        # means = torch.sum(torch.softmax(mean_weight, dim=2) * self.base_X, dim=2)  #[1, K, d]
-        means = self.mean_weight(y).reshape((-1, self.K, self.x_size))  # [N, K, d]
-        return means
-
-    def logpdf(self, x, y):  # H(X|Y)
-        # for data in (x, y):
-        # assert len(data.shape) == 2 and data.shape[1] == self.d, 'x has to have shape [N, d]'
-        # assert x.shape == y.shape, "x and y need to have the same shape"
-
-        x = x[:, None, :]  # [N, 1, d]
-
-        w = torch.log_softmax(self.weight(y), dim=-1)  # [N, K]
-        std = self.std(y).exp()  # [N, K]
-        # std = self.std(y)  # [N, K]
-        mu = self._get_mean(y)  # [1, K, d]
-
-        y = x - mu  # [N, K, d]
-        y = std ** 2 * torch.sum(y ** 2, dim=2)  # [N, K]
-
-        y = -y / 2 + self.d * torch.log(torch.abs(std)) + w
-        y = torch.logsumexp(y, dim=-1)
-        return self.logC + y
-
-    def pdf(self, x, y):
-        return torch.exp(self.logpdf(x, y))
-
-    def forward(self, x, y):
-        z = -self.logpdf(x, y)
-        return torch.mean(z)
-
-
-
-# CLEAN Differential entropy
-
-
-class KNIFE(nn.Module):
-    def __init__(self, args, zc_dim, zd_dim):
-        super(KNIFE, self).__init__()
-        self.kernel_marg = MargKernel(args, zc_dim, zd_dim)
-        self.kernel_cond = CondKernel(args, zc_dim, zd_dim)
-
-    def forward(self, z_c, z_d):  # samples have shape [sample_size, dim]
-        marg_ent = self.kernel_marg(z_d)
-        cond_ent = self.kernel_cond(z_c, z_d)
-        return marg_ent - cond_ent, marg_ent, cond_ent
-
-    def learning_loss(self, z_c, z_d):
-        marg_ent = self.kernel_marg(z_d)
-        cond_ent = self.kernel_cond(z_c, z_d)
-        return marg_ent + cond_ent
-
-    def I(self, *args, **kwargs):
-        return self.forward(*args[:2], **kwargs)[0]
-
-
-class MargKernel(nn.Module):
+class MarginalKNIFE(nn.Module):
     def __init__(self, args, zc_dim, zd_dim, init_samples=None):
 
         self.optimize_mu = args.optimize_mu
@@ -659,7 +597,7 @@ class MargKernel(nn.Module):
         self.d = zc_dim
         self.use_tanh = args.use_tanh
         self.init_std = args.init_std
-        super(MargKernel, self).__init__()
+        super(MarginalKNIFE, self).__init__()
 
         self.logC = torch.tensor([-self.d / 2 * np.log(2 * np.pi)])
 
@@ -796,7 +734,3 @@ class ConditionalPDF(nn.Module):
         mu, ln_var = torch.split(self.X2Y(X), self.dim, dim=1)
         cross_entropy = compute_negative_ln_prob(Y, mu, ln_var, self.pdf)
         return cross_entropy
-
-
-
-
