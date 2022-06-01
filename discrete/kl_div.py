975eb3b8ce1d478a9915ebb1469c647db422ebf3
MarinePICOT <picotmarine@gmail.com>
2022-06-01 10:11:17 -0400

    Update kl_div.py

--- discrete/kl_div.py
+++ discrete/kl_div.py
@@ -11,10 +11,6 @@ class KullbackLeiblerDivergence(DiscreteEstimator):
         :param hypo_dist: discreate hypothesis reference distribution over the vocabulary
         :return: fisher rao distance between the reference and hypothesis distribution
         """
-        fisher_rao = torch.clamp(
-            torch.sum(torch.sqrt(X) * torch.sqrt(
-                Y),
-                      dim=-1), 0, 1)
-        return 2 * torch.acos(fisher_rao)
+        return torch.sum(X * log(X / Y), dim=1)
 
 
